d6ede031c251fe5b0f28e4175b7ccd7254ecb667
abeyuyajp <yellowmonkey0618@gmail.com>
2021-12-06 23:45:26 +0900

    feat:🚀共通レイアウトを作成

---
+++ backend/resources/js/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="navbar">
+    <RouterLink class="navbar__brand" to="/">
+      Lockin
+    </RouterLink>
+    <div class="navbar__menu">
+      <div class="navbar__item">
+        <button class="button">
+          <i class="icon icon-md-add"></i>
+          投稿する
+        </button>
+      </div>
+      <span class="navbar__item">
+        username
+      </span>
+      <div class="navbar__item">
+        <RouterLink class="button button--link" to="/login">
+          ログイン / 新規登録
+        </RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Auth::routes();
+//Auth::routes();
 
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
 
